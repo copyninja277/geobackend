ab4acea8ead6ac30cb6b9b4a3a4993a29dc56ae9
copyninja277 <150160262+copyninja277@users.noreply.github.com>
2025-06-27 23:16:46 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,11 +9,4 @@ const UserSchema = new mongoose.Schema({
   otpExpiry: { type: Date }, // OTP expiry time
 });
 
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
 module.exports = mongoose.model('User', UserSchema);
